0e94dcf00845711b5a0effd7961467b5777917c1
kthompson5 <111643299+kthompson5@users.noreply.github.com>
2025-07-04 14:15:18 -0500

    main.js

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+// Get weather + OEAI
+fetch('/api/weather')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('comfort-message').innerText = data.comfort_message;
+        document.getElementById('temp').innerText = data.temp_f;
+        document.getElementById('humidity').innerText = data.humidity;
+        document.getElementById('wind').innerText = data.wind_speed;
+        document.getElementById('cloud').innerText = data.cloud_cover;
+    });
+
+// Create Leaflet map
+const map = L.map('map').setView([35.5062, -97.7668], 16);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Placeholder: Add a marker at Yukon Bible Church
+L.marker([35.5062, -97.7668]).addTo(map)
+    .bindPopup('Yukon Bible Church')
+    .openPopup();
